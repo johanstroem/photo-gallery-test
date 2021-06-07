5ed6a63a305d9b8f9962d7914000d5675edc6c7d
Johan <johan.strom@cygni.se>
2021-06-07 19:30:29 +0200

    add hover effect

--- client/src/components/gallery/PersonCard.tsx
+++ client/src/components/gallery/PersonCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, ReactNode } from 'react'
 import styled from 'styled-components'
 import { Person } from '../../service/photos'
 import { FlexColumn } from '../layout'
@@ -6,11 +6,21 @@ import { FlexColumn } from '../layout'
 type PersonCardProps = {
   person: Person
 }
+const Overlay = ({children}: {children: ReactNode} ) => (
+  <OverlayContainer>
+    {children}
+  </OverlayContainer>
+)
 
 function PersonCard({ person }: PersonCardProps) {
+
+  const [hover, setHover] = useState(false)
   return (
     <PersonCardContainer>
-      <Photo src={person.photo.large} />
+      <PhotoContainer onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+        <Photo src={person.photo.large}/>
+        { hover && (<Overlay> <PersonEmail>{person.email}</PersonEmail> </Overlay>) }
+      </PhotoContainer>
       <PersonInfo>
         <PersonName>
           {person.name}
@@ -24,15 +34,18 @@ function PersonCard({ person }: PersonCardProps) {
 
 const PersonCardContainer = styled(FlexColumn)`
   padding: 12px;
-  max-width: 256px;
+  width: 90%;
+  margin: auto;
+  overflow: hidden;
+  align-items: center;
 `  
   
 const Photo = styled.img`
-  border-radius: 128px;
+  border-radius: 64px;
 `
 
 const PersonInfo = styled.div`
-  display: flex;1.1
+  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
@@ -42,5 +55,31 @@ const PersonInfo = styled.div`
 const PersonName = styled.h3``
 
 const PersonAge = styled.p``
+
+const PhotoContainer = styled.div`
+  position: relative;
+  width: 128px;
+`
+
+const OverlayContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: black;
+  height: 100%;
+  width: 100%;
+  opacity: 60%;
+  border-radius: 64px;
+  transition: all 0.4s ease-in-out 0s;
+  display: flex;
+  align-items: center;
+`
+const PersonEmail = styled.p`
+  color: white;
+  font-weight: bold;
+  overflow-wrap: break-word;
+  max-width: 128px;
+
+`
 
 export default PersonCard
